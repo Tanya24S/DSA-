caa05874a9955f9855aacb8b3cd42c657fc2a8cb
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-01-21 14:16:05 +0530

    Create Patterns.cpp

---
+++ Patterns.cpp
@@ -0,0 +1,77 @@
+//Solving patterns
+/* 1) ****
+      ****
+      ****
+      ****  */
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0; i<=3; i++){
+        cout<<"****"<<endl;
+    }
+}
+/* 2) *
+      **
+      ***
+      ****  */
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0; i<=3; i++){
+        for(int j=0; j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+/* 3) 1
+      12
+      123
+      1234  */
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=1; i<=4; i++){
+        for(int j=1; j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+/* 4) 1
+      22
+      333
+      4444  */
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=1; i<=4; i++){
+        for(int j=1; j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+/* 5) ****
+      ***
+      **
+      *  */
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=1; i<=4; i++){
+        for(int j=4; j>=i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+/* 6) 1234
+      123
+      12
+      1  */
