76423de5a153ce1fb698fd4bc35b4616b275b58a
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-02-03 10:31:15 +0530

    Create Check if sorted, remove duplicates.cpp

---
+++ Arrays/Check if sorted, remove duplicates.cpp
@@ -0,0 +1,26 @@
+//Check if sorted
+//1.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={3,4,7,7,9};
+    int c=0;
+    for(int i=0; i<=3; i++){
+        if(arr[i]>arr[i+1]){
+            break;
+        }
+        c++;
+        cout<<c;
+    }
+    cout<<c;
+    if(c==4){ cout<<"Array is sorted";}
+    else{ cout<<"Array is not sorted";}
+}
+//2. Check if sorted and rotated    Leetcode problem:1752
+
+
+
+
+//3.Remove duplicates from sorted array   Leetcode problem:26
+
