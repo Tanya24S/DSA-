efe5f7500fa79ac1d8686e847568608f4a95ff35
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-02-16 19:04:07 +0530

    Create Dynamic programming Basics.cpp

---
+++ Dynamic Programming/Dynamic programming Basics.cpp
@@ -0,0 +1,10 @@
+//DP parent problem recursion, wherever overlapping Recursion, we'll apply DP
+//How to identify problem:
+//1. Choice given in question to keep or eliminate the element
+//2. To optimize in problem(either maximize or minimize)
+
+//Approach: Recursive solution-> Memoization-> Top Down Dp
+//1. Knapsack Problem(Bag)
+// 3 types-> Fractional Knapsack(not part of DP, comes under greedy algorithm), 0/1 Knapsack, Unbounded Knapsack
+
+//DP recurive code
