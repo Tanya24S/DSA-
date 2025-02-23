41370e95cf80933afb146c5a63e8cd72a8d1581e
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-02-23 13:46:16 +0530

    Create Important questions.cpp

---
+++ Important questions.cpp
@@ -0,0 +1,10 @@
+//Problem 1:(Asked in google girl hackathon OA)
+//Your are given two strings S and R, you can perform two operations: Type1->select two indices and swap. This operation is free of cost. 
+//Type2-> Select any index and swap/change with any english letter. This costs one coin.  //Minimize the cost to make S is equal to R
+
+
+
+//Problem 2: Sample coding question(google), Leetcode: 2239
+//In a given array of numbers find the number closest to zero(-ve numbers allowed), and if there are more than one element than select the one greater.
+
+
