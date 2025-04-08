d4985b410b11ba7c3360fb44c09b70f034bbc58c
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-04-08 10:48:39 +0530

    BS implementation.cpp

--- Binary search/BS implementation.cpp
+++ Binary search/BS implementation.cpp
@@ -18,4 +18,5 @@ public:
         return -1;
     }
 };
-//
+//Note: Instead of mid=(low+high)/2, we must write mid=low+(high-low)/2, this is done to avoid int overflow, otherwise in cases
+//where low and mid summation exceeds the limit then we might get wrong mid(in most cases negative number).
