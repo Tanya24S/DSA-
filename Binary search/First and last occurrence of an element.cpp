6e9849d41408b619720fd2f1be683f548bc6a229
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-04-14 01:24:43 +0530

    First and last occurrence of an element.cpp

--- Binary search/First and last occurrence of an element.cpp
+++ Binary search/First and last occurrence of an element.cpp
@@ -1,4 +1,5 @@
 //First approach(binary search+ linear search)
+//Note:I first brainstorm and write code on my own then optimize correct solution
 #include <iostream>
 #include <string>
 #include <vector>
@@ -40,8 +41,8 @@ int main(){
     int i=index.size()-1;
     cout<<"["<<index[i]<<", "<<index[0]<<"]";
     return 0;
-} 
-//Second approach(more optimized approach)
+} //this code has one issue, that if mid is the target then it's index will be the first pushback and our index vector will not return us the sorted version or the first occurence index exactly
+//Second approach(much better)
 #include <iostream>
 #include <string>
 #include <vector>
@@ -76,3 +77,44 @@ int main(){
     return 0;
 } 
 //Leetcode problem 34:
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> index;
+        int res=-1;
+        int tes=-1;
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                res=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        index.push_back(res);
+        low=0;
+        high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                tes=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        index.push_back(tes);
+        return index;      
+    }
+};
