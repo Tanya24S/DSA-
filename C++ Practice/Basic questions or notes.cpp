d4ca8ee8da0decb8bd535247350f3f08bde554a5
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-04-17 21:55:27 +0530

    Basic questions or notes.cpp

---
+++ C++ Practice/Basic questions or notes.cpp
@@ -0,0 +1,3 @@
+//pass by value and pass by reference, 1)In pass by value, we just inherit the value and make a new copy of it, 2) In pass by reference we want og to get edited
+//so we pass a reference to it as string& s
+//Size of Fundamental Data Types
