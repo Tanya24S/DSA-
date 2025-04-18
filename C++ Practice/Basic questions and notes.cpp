2f0dac5987fedb0df46c60afb155516c17ba05b1
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-04-18 10:05:47 +0530

    Basic questions and notes.cpp

---
+++ C++ Practice/Basic questions and notes.cpp
@@ -0,0 +1,5 @@
+//pass by value and pass by reference, 1)In pass by value, we just inherit the value and make a new copy of it, 2) In pass by reference we want og to get edited
+//so we pass a reference to it as string& s (pass by value will not be the ebst for big data structures with large values, more memor consumption)
+//C++in stl: If write const in front of the vector then it won't get updated, read only mode, use push_back() to add elements in the vector
+//Size of Fundamental Data Types
+sizeof()
