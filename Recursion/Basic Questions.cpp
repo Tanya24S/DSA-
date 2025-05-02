e3311e27b9b970d782ae19bc6727917ddc60aaca
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-05-02 23:47:21 +0530

    Update Basic Questions.cpp

--- Recursion/Basic Questions.cpp
+++ Recursion/Basic Questions.cpp
@@ -1,4 +1,5 @@
 //Print name N-times
+//Code:1
 #include <iostream>
 using namespace std;
 
@@ -18,3 +19,28 @@ int main() {
     cin>>s;
     names(s,n);
 }
+//Code:2    //Now why code-2 is significantly better is because here it dosen't rely on global variable, so it's not contained within the function making is messier to work with, it is not encouraged to use global avriables during recursion calls to keep the track of calls,
+//In the first code though the numebr of calls are contained within the function itself, which makes it easier and cleaner to debug/work with.
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print_names(string s, int n){
+    if(n==0) return;
+    print_names(s,n-1);
+    cout<<s<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    cout<<"Enter the name:";
+    string s;
+    cin>>s;
+    print_names(s,n);
+    return 0;
+}
+//Print Linearly from 1-N
+
+//Print from N to 1
