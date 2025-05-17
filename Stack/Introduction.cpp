9cdaea0ad5c9a9f24872501650ffd581ce7f4dbd
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-05-17 12:39:45 +0530

    Introduction.cpp

---
+++ Stack/Introduction.cpp
@@ -0,0 +1,4 @@
+//How to Identify if it's a stack problem:
+//1. It'll be an array question (usually for array question you apply either stack or heap but with heap you'll feel like sorting)
+//2. If you have written brute force solution of O(n^2) where we have depended loop, where j(nested loop) value depends on i, here you can write a better solution using stack.
+//First identify the pattern(from those you have studied) then if there can be any variation from it.
