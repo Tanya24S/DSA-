93bc8cd1e8412cd8e28597289cc4d989a932fe62
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-06-14 11:08:55 +0530

    Update Random questions.cpp

--- C++ Practice/Random questions.cpp
+++ C++ Practice/Random questions.cpp
@@ -20,3 +20,50 @@ int main(){
     cout<<a;
 }//Instead of doing this I can just sum using i  as a+=i;
 //Using formula return (n*(n+1))/2
+//So I got to know that formula for first n natural numbers is called Gauss's formula
+
+//3. Reverse an array
+//using two pointer technique:
+#include <iostream>
+using namespace std;
+
+void reversearray(int arr[], int n){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    }
+}
+
+int main(){
+    int arr[7]={3,2,1,3,3,2,3};
+    int n=7;
+    reversearray(arr, n);
+    for(int i=0; i<7; i++){
+        cout<<arr[i];
+    }
+}
+//using loop
+#include <iostream>
+using namespace std;
+
+void reversearray(int arr[], int n){
+    int a=n-1;
+    for(int i=0; i<=(n/2); i++){
+        swap(arr[i],arr[a-i]);
+    }
+}
+
+int main(){
+    int arr[7]={3,2,1,3,3,2,3};
+    int n=7;
+    reversearray(arr, n);
+    for(int i=0; i<7; i++){
+        cout<<arr[i];
+    }
+}
+
+
+
