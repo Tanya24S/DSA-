31142290f34a9bee3aa64562d046cc630117ed66
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-06-19 03:23:43 +0530

    Update Extract and Count digits.cpp

--- Basic maths or Special number problems/Extract and Count digits.cpp
+++ Basic maths or Special number problems/Extract and Count digits.cpp
@@ -12,26 +12,33 @@ int main(){
         n=n/10;
     }
 }
-//Extract digits in non reverse:
-
+//For extracting in non-reverse manner we can either reverse it and then extract or convert it to string and then print from firest index.
 //Calculate number of digits in an integer
 #include <iostream>
-#include <string>
+using namespace std;
+
+int countdigits(int n){
+    int temp=n;
+    int c=0;
+    while(temp>0){
+        temp=temp/10;
+        c++;
+    }
+    return c;
+}
+
+int main(){
+    int n=7485961;
+    cout<<countdigits(n);
+}
+//Another method to calculate the number of digits in an integer(use of log10(n))
+#include <iostream>
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-    int c=0;
-    while(n>0){
-        int a=n%10;
-        cout<<a<<endl;
-        n=n/10;
-        c++;
-    }
-    cout<<"Number of digits in the integer:"<<c;
+    int n=7485961;
+    int digit=log10(n)+1;
+    cout<<digit;
 }
-//Another method to calculate the number of digits in an integer(use of log10(n))
-
 
   
