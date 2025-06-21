f2f7d921621cfcd02a09ec73e5962f4dcc097b1f
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-06-22 00:11:08 +0530

    Update Perfect number.cpp

--- Basic maths or Special number problems/Perfect number.cpp
+++ Basic maths or Special number problems/Perfect number.cpp
@@ -1,1 +1,28 @@
-//
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool perfectnumber(int a){
+    int temp=a;
+    int n=sqrt(temp);  //To find all divisors we traverse till square root of that number
+    int res=1;
+    for(int i=2; i<=n; i++){
+        if(temp%i==0){
+            res+=i;
+            int d=temp/i;  //other divisors after the sqrt root value can be determined like this
+            res+=d;
+        }
+    }
+    cout<<res<<endl;
+    if(res==temp) return true;
+    else return false;
+    
+}
+
+int main(){
+    int a;
+    cout<<"Enter the number:";
+    cin>>a;
+    cout<<perfectnumber(a);
+    return 0;
+}
