a0b0e79c773b1ce7b0816ce879062de7a0534ec8
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-07-04 18:34:49 +0530

    Create Theory.cpp

---
+++ Time complexity and space complexity/Theory.cpp
@@ -0,0 +1,5 @@
+/* Theta- average case tc
+   omega- best case tc
+   bigO- worst case tc (the real one)
+*/
+//Very imp: 10^8 operations are usually allowed by an online compiler otherwise you'll get a TLE
