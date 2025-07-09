ca20c0329389f475169584ed0b5cf676008be31e
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-07-09 12:02:34 +0530

    Update Introduction.cpp

--- Stack/Introduction.cpp
+++ Stack/Introduction.cpp
@@ -1,4 +1,8 @@
-//How to Identify if it's a stack problem:
+//How to Identify if it's a stack problem: (Aditya verma playlist)
 //1. It'll be an array question (usually for array question you apply either stack or heap but with heap you'll feel like sorting)
 //2. If you have written brute force solution of O(n^2) where we have depended loop, where j(nested loop) value depends on i, here you can write a better solution using stack.
 //First identify the pattern(from those you have studied) then if there can be any variation from it.
+//_______________________________________________________________________________________________________________________________________________________________________________
+// (Geeks for geeks offline course)
+//To implement any solution that requires you to remove elements from between adn thenr estack them without any gap, usuallyw e use stack for such problems
+//Also, to implement insertion, access the last element, as well remove the last element, for such operation we'll use stack that has LIFO implementation (Last in first out)
