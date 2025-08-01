ac9080861c659c363d5e39a8f6cddab760198bee
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-02 03:45:32 +0530

    Theory+ basic problem.cpp

---
+++ Greedy Algorithm/Theory+ basic problem.cpp
@@ -0,0 +1,7 @@
+//Greedy algorithm is basically about optimizing that is either minimize or maximize the output, it is purely based on inuition, 
+//that is choosing the best in the moment and in every step.
+
+//To cross verify, you have two sets one with the answers you chose and other with latter, now you verify is any choice from the right could make my answer better if no, correct sol.
+
+//Suppose you have given a set of coing and a certain amount you need to meet with minimum number of coins
+//int coins[], int amount, int n (number of elements in array coins)
