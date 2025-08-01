a333499a685b1f002abffc26166f10cee1d9c4d3
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-01 18:28:45 +0530

    Update Maximize Toys.cpp

--- Greedy Algorithm/Maximize Toys.cpp
+++ Greedy Algorithm/Maximize Toys.cpp
@@ -1,1 +1,20 @@
 //Gfg: https://www.geeksforgeeks.org/problems/maximize-toys0331/1
+class Solution {
+  public:
+    int toyCount(int N, int K, vector<int> arr) {
+        sort(arr.begin(), arr.end());
+        int i=0;
+        int c=0;
+        while(K>=arr[i]){
+            K=K-arr[i];
+            c++;
+            if(i<=arr.size()-2){
+                i++;
+            }
+            else{ break;}
+        }
+        return c;
+        
+    }
+}; 
+
