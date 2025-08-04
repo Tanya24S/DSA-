93ff248e9037c878d2e387b7c939f4eec0c625b8
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-04 19:34:51 +0530

    Theory+Basics.cpp

---
+++ Trees/Theory+Basics.cpp
@@ -0,0 +1,10 @@
+//Trees are like family trees, another real life example would be drop down menu on websites, that requires tree structure
+//Terms: root (top most given node), levels (basically how many generations are there etc), sibings (child nodes from same parent), parent nodes
+    // leaf nodes (last noeds with no child nodes), LCA (Lowest common ancestor)
+//Types of trees: 1. Binary trees (every node can have 0-2 nodes), 2. Teranary trees (every node can have 0-3 nodes), 3. N-aray trees (every nodes can have 0-n nodes)
+
+//Skewed Binary trees
+//1. Left skewed   2. Right skewed
+//Complete Binary trees
+
+//Implementation of a binary tree
