4fd77f10aad174d18c2b4b00316ad42f6e5e982d
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-11 23:38:04 +0530

    Update Theory+ basic problem.cpp

--- Greedy Algorithm/Theory+ basic problem.cpp
+++ Greedy Algorithm/Theory+ basic problem.cpp
@@ -3,5 +3,5 @@
 
 //To cross verify, you have two sets one with the answers you chose and other with latter, now you verify is any choice from the right could make my answer better if no, correct sol.
 
-//Suppose you have given a set of coing and a certain amount you need to meet with minimum number of coins
+//Suppose you have given a set of coins and a certain amount you need to meet with minimum number of coins
 //int coins[], int amount, int n (number of elements in array coins)
