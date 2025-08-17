23c0722025122ee465fdd96e1bcadd9607682593
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-17 09:28:58 +0530

    Basic.cpp

---
+++ Trees/Binary Search Tree/Basic.cpp
@@ -0,0 +1,7 @@
+//Unique values(all values are unique, there are no duplicate values in a BST)
+//RIght hand side values are always greater than the root while left hadn side values are always less than the root.
+
+//The distance between left adn right height should not be gretaer than 1 for all the nodes: Binary search tree.
+//Inorder Traversal of a BST always gives a sorted result.
+
+//Check if 
