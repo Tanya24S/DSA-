a5248ff4c38d6977e695046b1ae3bda1a32f7735
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-22 18:20:55 +0530

    Basics.cpp

--- Graph/Basics.cpp
+++ Graph/Basics.cpp
@@ -1,2 +1,3 @@
 //graphs can be visualised as network of roads and cities basically cities are nodes and roads are edges.
-//they can be represented using two adjacency matric and adjacency list
+//they can be represented using either adjacency matrix or adjacency list
+//We have two types of graphs 1. Directed graphs and 2. Undirected graphs
