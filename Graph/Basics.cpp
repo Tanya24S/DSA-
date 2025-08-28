cdf2f297c391c64d9d58e0870bc0f04e7ffbbf17
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-29 02:10:22 +0530

    Update Basics.cpp

--- Graph/Basics.cpp
+++ Graph/Basics.cpp
@@ -22,9 +22,9 @@ int main() {
     }
 
     cout << "Edges list:\n";
-    for (int i = 0; i < edges; i++) {
-        for (int j = 0; j < (int)edges_list[i].size(); j++) {
-            cout << edges_list[i][j] << " ";
+    for (int i = 0; i < edges; i++) {   
+        for (int j = 0; j < (int)edges_list[i].size(); j++) {    //a better way would be using for(int ng: edges_list[i]), cout<<ng
+            cout << edges_list[i][j] << " ";                                               //(this was just me using brute force approach)
         }
         cout << "\n";
     }
@@ -32,11 +32,43 @@ int main() {
     cout << "\nAdjacency list:\n";
     for (int i = 0; i < nodes; i++) {
         cout << i << ": ";
-        for (int j = 0; j < (int)adj[i].size(); j++) {
+        for (int j = 0; j < (int)adj[i].size(); j++) {   //similarly here for(int ng: adj[i])
             cout << adj[i][j] << " ";
         }
         cout << "\n";
     }
 
+    return 0;
+}
+//Adjacency matrix
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int nodes,edges;
+    cin>>nodes>>edges;
+    vector<vector<int>> edges_list(edges);
+    int matrix[nodes][nodes]={0};
+    for(int i=0; i<edges; i++){
+        int x, y;
+        cin>>x>>y;
+        edges_list[i]={x,y};
+        matrix[x][y]=1;
+        matrix[y][x]=1;
+    }
+    for(int i=0; i<edges; i++){
+        for(int x: edges_list[i]){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0; i<nodes; i++){
+        for(int j=0; j<nodes; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
