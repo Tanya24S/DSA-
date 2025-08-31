06954d676b9569ea2957bd03ebf86757db087a85
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-08-31 20:24:14 +0530

    Interview questions.cpp

---
+++ Leetcode problems/Similar questions asked in OA, Interviews/Interview questions.cpp
@@ -0,0 +1,22 @@
+//Amazon Interview on-campus 2025 (There was only one technical round)
+//I took a lot of time to reach the approach but I did and then I coded it but it had a few errors in it :(
+//A lot of people were asked textbook questions like: Height of a binary tree, Twisted questions from Binary search, How to implement a min-heap? etc
+//Some were asked questions like: Minimum platforms required for trains with overlapping schedules
+//I was asked Question similar to problem Leetcode 2226: https://leetcode.com/problems/maximum-candies-allocated-to-k-children/description/
+//Understanding:
+//Code:
+
+
+
+//Citi Interview on-campus 2024 (I was able to clear technical round and gave hr round)
+//I was not asked any coding question but I was asked questions from cs subjects
+//Give real life examples of Queue, tree, graphs, stacks, arrays etc
+Answers: Any line in store or queue for tickets[Queue], Family tree[Trees], Roads and cities connected on maps[Graphs], Hanoi tower[Stack]
+//Questions about my CV like: What is Unity 3D, how do you make games on it? How did you implement OOPs concepts in C#?
+//Some questions from DBMS, SQL and OOPS
+
+
+
+
+
+
