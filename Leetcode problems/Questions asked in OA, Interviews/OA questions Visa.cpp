860825af966607ca1f9cecd3c8ad57866a77c2c6
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-03 19:07:14 +0530

    OA questions Visa.cpp

---
+++ Leetcode problems/Questions asked in OA, Interviews/OA questions Visa.cpp
@@ -0,0 +1,32 @@
+//Ques 1: The question was something like I was given an array of integers and i need to check if they are monotonic as i, i+1, i+2 either increasing or decreasing and return a
+//array/vector with 0 and 1 if it was not or was respectively.
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+vector<int> check_monotonic(vector<int> arr, vector<int> &result){
+    if(arr.size()<3){
+        return {0};
+    }
+    for(int i=0; i<arr.size()-2; i++){
+        if(((arr[i]<arr[i+1]) && (arr[i+1]<arr[i+2])) || ((arr[i]>arr[i+1]) && (arr[i+1]>arr[i+2]))){
+            result.push_back(1);
+        }
+        else result.push_back(0);
+    }
+    return result;
+}
+int main(){
+    vector<int> arr={10, 10, 10, 10, 10};
+    vector<int> result;
+    check_monotonic(arr, result);
+    for(int i=0; i<result.size(); i++){  //the compiler is giving me a warning though even if the code is running fine because of unsigned and int comparison.
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
+//Ques2: 
