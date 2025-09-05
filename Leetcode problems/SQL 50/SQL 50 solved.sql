222a94fc0f4d171f94fbe11f89bea2ac24fe7971
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-05 19:32:05 +0530

    Update SQL 50 solved.sql

--- Leetcode problems/SQL 50/SQL 50 solved.sql
+++ Leetcode problems/SQL 50/SQL 50 solved.sql
@@ -11,3 +11,8 @@ SELECT name, population, area FROM World WHERE population>=25000000 OR area>=300
 select distinct author_id as id from views where viewer_id=author_id order by author_id ASC;
 
 --BASIC JOINS
+--1378. Replace Employee ID With The Unique Identifier
+select eu.unique_id, e.name from Employees e left join EmployeeUNI eu on e.id=eu.id;
+--1068. Product Sales Analysis I
+select pr.product_name, sl.year, sl.price from Sales sl inner join Product pr on pr.product_id=sl.product_id;
+--1581. Customer Who Visited but Did Not Make Any Transactions
