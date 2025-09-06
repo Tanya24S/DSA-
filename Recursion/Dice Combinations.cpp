a97cb9d1abfef78b6c73aa215f173f4749be60bb
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-06 23:15:52 +0530

    Dice Combinations.cpp

--- Recursion/Dice Combinations.cpp
+++ Recursion/Dice Combinations.cpp
@@ -1,1 +1,4 @@
 //This recursive solution will give TLE and can be solved properly using dp, but to understand recursion we can use this problem
+//Given number find possible ways we can find digits of a dice to add up to it.
+//Pruning: When we remove the unwanted calls in recursion to optimise recursion
+//Code:
