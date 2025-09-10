6e9d93913d7abaed3cc3cefcbb845e91a819ffa4
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-10 11:19:00 +0530

    Update Interview Questions.cpp

--- Leetcode problems/Questions asked in OA, Interviews/Interview Questions.cpp
+++ Leetcode problems/Questions asked in OA, Interviews/Interview Questions.cpp
@@ -1,4 +1,4 @@
-//Amazon Interview on-campus 2025 (There was only one technical round)
+//Amazon Interview on-campus 2025 (SDE intern+ppo) (There was only one technical round)
 //I took a lot of time to reach the approach but I did and then I coded it but it had a few errors in it :(
 //A lot of people were asked textbook questions like: Height of a binary tree, Twisted questions from Binary search, How to implement a min-heap? etc
 //Some were asked questions like: Minimum platforms required for trains with overlapping schedules
@@ -7,8 +7,25 @@
 //Code:
 
 
+//PhonePe on-campus 2025 (Graduate Trainee) (I was able to reac till the second last interview)
+//Round-1-> Group discussion: Topics-> Is fintech disruption to tradition banking or it complements it? (out of 300+ students 146 were shortlisted for gd)
+//I made a few points like: We are still linked to banks regardless only the money flow has changed from cash to online payment, the cash flow pipeline is still the same
+//The only disruption is probably security issues that comes with it, cybersecurity and other threats
+//Also, we are no where near in the rpesent time getting rid of traditional offline banks, as their requirement is still vital, I gave an example of how I recently had
+//to go to bank physically to get my bank number changed and linked it with aadhar(something like that)
+//In gd people wanna bite you, lol, that's how it looks like
+//Round-2-> Technical Interview: (out of 146 students 40 were shortlisted for interview)
+I was asked to give my introduction, then why join phonepe when my branch was ece-ai, then he asked me what is sql union,join
+Then he asked me a few puzzles like: If an ant is on one vertice of a cube and needs to reach on the vertice of opposite face but can't fly diagonally that is root 3 then what
+would be the shortest path: I said roo2+length but apparently that wasn't the answer, then he asked me if youa re travelling in metro how will you tell number of people
+in that train-> Answer could be that we know initial weight of the train, we can know the weight increased and take an average weight and divide to know approx how many people are there,
+then he asked me how to know how many people in metro station or said there are multiple lines right, I didn't understand honestly -> I said we'll just use a counter of how many people
+came in through the gates and entered through cards/qr (but apparently he was looking for a different answer)
+Lastly, he asked me what is the project I like the most in my resume and to explain that and a few questions from that
+//The role was a bit non-tech and I wanted to pursue tech, hence I was a little skeptical abt it
 
-//Citi Interview on-campus 2024 (I was able to clear technical round and gave hr round)
+
+//Citi Interview on-campus 2024 (SDE intern) (I was able to clear technical round and gave hr round)
 //I was not asked any coding question but I was asked questions from cs subjects
 //Give real life examples of Queue, tree, graphs, stacks, arrays etc
 Answers: Any line in store or queue for tickets[Queue], Family tree[Trees], Roads and cities connected on maps[Graphs], Hanoi tower[Stack]
