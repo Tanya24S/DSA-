79331d48ed0b9b38a054f65b539cfe67f8ced15b
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-13 01:44:35 +0530

    Create Note.cpp

---
+++ Note.cpp
@@ -0,0 +1,6 @@
+//Note:
+/* If elements are not changing and we require first max, second max or first smallest, second smallest etc then you can use sorting.
+   Else if elments are dynamic and can be changed, then maybe use min heap/max heap according to requirement.*/
+
+/*Whenever we want custon sorting like sorting pairs and pair is a class then we use comparators, you may either use your own sorting or comparators which won't take time
+  How it works? It basically changes to if condition in sorting algorithm*/
