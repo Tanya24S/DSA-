898399c96d359b7afce4adc99ac575abe8973273
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-17 17:03:50 +0530

    Update Fibonacci Number.cpp

--- Recursion/Fibonacci Number.cpp
+++ Recursion/Fibonacci Number.cpp
@@ -1,1 +1,20 @@
-//Leetcode Problem 509:Fibonacci Number
+//Leetcode Problem 509: Fibonacci Number
+//Code 1:
+class Solution {
+public:
+    int rec(int n, int &answer){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        answer=rec(n-1,answer)+rec(n-2,answer);
+        return answer;
+    }
+    int fib(int n) {
+        int answer=0;
+        rec(n, answer);
+        if(n==1) return 1;
+        else return answer;
+    }
+};
+//Code 2 optimized using dp:
+//Dynamic programming is like cache hit or miss
+
