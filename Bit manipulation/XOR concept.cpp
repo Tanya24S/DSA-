80d124229894857f485f834860893e44b365e6ba
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-19 00:35:13 +0530

    Create XOR concept.cpp

---
+++ Bit manipulation/XOR concept.cpp
@@ -0,0 +1,9 @@
+/* XOR (^ in C++) is a bitwise operation.
+For each bit position:
+0 ^ 0 = 0
+1 ^ 1 = 0
+0 ^ 1 = 1
+1 ^ 0 = 1
+So XOR gives 1 if the bits are different, 0 if they’re the same. */
+//x ^ x = 0 (any number XORed with itself cancels out).
+//x ^ 0 = x.
