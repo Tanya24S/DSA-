5d3da87dd37f03ff2ba620df584022d6365551d7
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-21 17:51:30 +0530

    Create Peak element.cpp

---
+++ Binary search/Peak element.cpp
@@ -0,0 +1,4 @@
+//Binary search on answer concept
+//Here, we just change the criteris through which we check our mid rest remains same, also so far we knew BS is applied on sorted array, here it can be applied on unsorted array as well.
+//Peak element (Leetcode 162)
+
