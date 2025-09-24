3e725d4112b0579992f9873d61be3eeaf9ce6d39
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-09-24 13:37:38 +0530

    Update 231- Power of two.cpp

--- Leetcode problems/231- Power of two.cpp
+++ Leetcode problems/231- Power of two.cpp
@@ -16,3 +16,11 @@ public:
     }
 };
 //Optimized approach using & operator:
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        else if((n&(n-1))==0) return true;
+        else return false;
+    }
+};
