96b03b02a1312ddf07d4d794d131390916d12703
Tanya Sharma <118129609+Tanya24S@users.noreply.github.com>
2025-10-07 19:26:55 +0530

    Create 136-Single Number.cpp

---
+++ Leetcode problems/136-Single Number.cpp
@@ -0,0 +1,37 @@
+//Code 1:  Tc:O(N)  SC:O(N)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        for(auto& i: mpp){
+            if(i.second==1){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
+//Code 2:          Tc: O(log N) SC=O(1)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums.size()==1) return nums[0];
+        if((nums[0]^nums[1])!=0){
+            return nums[0];
+        }
+        if((nums[nums.size()-1]^nums[nums.size()-2])!=0){
+            return nums[nums.size()-1];
+        }
+        for(int i=1; i<nums.size()-1; i++){
+            if((nums[i]^nums[i+1])!=0 && (nums[i]^nums[i-1])!=0){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+//Code 3:      
